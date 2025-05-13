8a71802df9b1c75ae774c88606236430a0cbb786
Ricardo Bermejo <mestreledark@gmail.com>
2025-05-13 16:21:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="refresh" content="0; url=https://www.linkedin.com/in/jhenny-mamani">
-    <title>Redirecionando...</title>
+    <title>Por favor, aguarde. Redirecionando...</title>
 </head>
 <body>
     <p>Se você não for redirecionado automaticamente, <a href="https://www.linkedin.com/in/jhenny-mamani">click aqui</a>.</p>
